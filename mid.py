db3ebaac22d6b36f1e43cb99e01f329667c95f2a
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-04-10 13:06:54 +0200

    Add files via upload

---
+++ mid.py
@@ -0,0 +1,13 @@
+def mid(txt):
+    return txt[len(txt) // 2] if len(txt) % 2 != 0 else ""
+
+"""
+learned:
+in line if statement:
+written like you read it
+
+return (var) if (condition) else (var)
+
+
+
+"""
