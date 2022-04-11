296889a8daa0cd53db4bf743d8c1dd007f93133c
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-04-11 09:59:30 +0200

    Add files via upload

---
+++ one_line_convert.py
@@ -0,0 +1,15 @@
+def convert(arr):
+    return [str(arr[i]) for i in range(len(arr))]
+
+def convert(arr):
+	newArr = []
+	for i in arr:
+		newArr.append(str(i))
+
+"""
+learned:
+one line for loops 
+are interesting 
+you define the var and the 
+change before the loop
+"""
