96790bf9bb4f935c4baa4be2fe192db113cf9d36
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-04-11 09:59:47 +0200

    Add files via upload

---
+++ online_status.py
@@ -0,0 +1,9 @@
+def online_count(dic):
+    return len([i for i in dic if dic[i] == "online"])
+
+"""
+learned:
+one line for loops
+[] get you an array 
+so you can count that array and not use a second var
+"""
